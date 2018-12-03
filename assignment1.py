a8a98f0c7360455e3b9a95540e2d59fd1accc8d6
Issac Chan <issaczhaoming.chan@my.jcu.edu.au>
2018-12-03 17:23:51 +0800

    test comment

--- assignment1.py
+++ assignment1.py
@@ -5,7 +5,7 @@ Replace the contents of this module docstring with your own details.
 
 def main():
     print("Songs To Learn 1.0 - by <Your Name>")
-
+    #testing comment
 
 if __name__ == '__main__':
     main()
