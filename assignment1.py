4d42b469fe8772650733c61d5583ac713516eba8
Issac Chan <issaczhaoming.chan@my.jcu.edu.au>
2018-12-07 19:42:51 +0800

    completed half of 'Complete a song' function

--- assignment1.py
+++ assignment1.py
@@ -21,16 +21,16 @@ def menu():
 
 def songsList():
     songRead = open("songs.csv", "r")
-    ListOfSongs = []
+    songs = []
     for element in songRead:
-        ListOfSongs.append(element.split(','))
-    for i in ListOfSongs:
+        songs.append(element.split(','))
+    for i in songs:
         i[3] = i[3].strip('\n')
 
     songCount = 0
     songsLearnt = 0
     songsToLearn = 0
-    for songElement in ListOfSongs:
+    for songElement in songs:
         if songElement[3] == 'n':
             songElement[3] = ('*')
             songsToLearn += 1
@@ -38,16 +38,30 @@ def songsList():
             songElement[3] = (' ')
             songsLearnt += 1
         songCount += 1
-        print("{}. {} {:40}-{:<30}({:10})".format(songCount, songElement[3], songElement[0], songElement[1],
+        print("{}. {} {:40}- {:<30}({:4})".format(songCount, songElement[3], songElement[0], songElement[1],
                                                   songElement[2]))
     print("{:} songs learned, {:} songs still to learn".format(songsLearnt, songsToLearn))
     songRead.close()
+    return songs
 
-def songsAdder():
+def songsAdder(songs):
     songAdd = open("songs.csv", "a+")
     songList = {}
 
-def songsComplete():
+def songsComplete(songs):
+    try:
+        songSelect = int(input("Enter the number of a song to mark as learned: "))
+        while songSelect < 0:
+            print("Number must be >= 0")
+            songSelect = int(input("Enter the number of a song to mark as learned: "))
+
+        if songs.songElement[3] == '*':
+            print("{} by {} learned".format(songs.songElement[0],songs.songElement[1]))
+        else:
+            print("You have already learned {}".format(songs.songElement[0]))
+    except ValueError:
+        songSelect = int(input("Invalid input; enter a valid number: "))
+
 
 def main():
     userName = str(input("Please enter your name: "))
